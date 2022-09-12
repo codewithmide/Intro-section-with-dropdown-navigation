84881c57df0c884a5a0a3517d9676a02897adda2
Olumide Micheal <olumidemicheal7@gmail.com>
2022-09-12 14:32:52 +0100

    fix the arrow directions

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ const featureDropdown = document.querySelector('.feature-dropdown');
 const companyDropdown = document.querySelector('.company-dropdown');
 const openArrow = document.querySelector('.open');
 const closeArrow = document.querySelector('.close');
+const companyOpenArrow = document.querySelector('.cmp-open-arrow');
+const companyCloseArrow = document.querySelector('.cmp-close-arrow')
 
 feature.addEventListener("click", () => {
     featureDropdown.classList.remove('hidden');
+    openArrow.style.display = "none"
+    closeArrow.style.display = "inline"
 })
 
 company.addEventListener("click", () => {
     companyDropdown.classList.remove('hidden');
+    companyOpenArrow.style.display = "none"
+    companyCloseArrow.style.display = "inline"
 })
